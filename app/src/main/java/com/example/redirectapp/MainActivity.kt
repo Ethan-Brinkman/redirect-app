1cd29fef98eeb6ed823295d9e8448d8122bd63cc
Ethan Brinkman <brinkmanethan@gmail.com>
2022-10-24 10:24:53 -0500

    Initial commit. Basic redirect capability.

---
+++ app/src/main/java/com/example/redirectapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.redirectapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        findViewById<View>(R.id.Timely_btn).setOnClickListener{
+            val url = "https://google.com"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+    }
+}
